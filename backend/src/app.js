b1a1f4c9cdd2546e8dbab306d0049e53e7c3741c
Metri Jain <your-github-email@example.com>
2026-06-04 16:45:51 +0530

    Add production frontend URL to CORS

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,9 @@ const app=express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin:["http://localhost:5173", "http://localhost:5174"],
+    origin:["http://localhost:5173",
+         "http://localhost:5174",
+        "https://interview-ai-0s8p.onrender.com"],
     credentials:true,
 })); 
 
